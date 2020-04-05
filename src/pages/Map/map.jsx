f770125ab780f82e20c2ea1a98c82d9d6359cbbc
XakimAA <malutok@mail.ru>
2020-04-06 00:06:16 +0500

    страница с картой

---
+++ src/pages/Map/map.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import mapboxgl from "mapbox-gl";
+
+import Header from "../../components/Header";
+import "./map.scss";
+
+const mainClass = "map";
+
+class Map extends Component {
+  map = React.createRef();
+  map_id = "map_order";
+
+  componentDidMount() {
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoic2F0YW5zZGVlciIsImEiOiJjanAwOGxqYnAyc3J4M3hucmJzaWh4OTg0In0.LSKzagFIJqivwgf4VFjC4Q";
+    this.mapbox = new mapboxgl.Map({
+      container: this.map_id,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [37.61167343575062, 55.75166954492968],
+      zoom: 12,
+    });
+  }
+
+  componentWillUnmount() {
+    this.mapbox.remove();
+  }
+
+  render() {
+    const { className } = this.props;
+    const classes = classNames(className, mainClass);
+    return (
+      <div>
+        <Header/>
+        <div id={this.map_id} className={`${mainClass}`} ref={this.map}></div>
+      </div>
+    );
+  }
+}
+
+export default Map;
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+export const pagesMap = {
+  map: {
+    id: "map",
+    href: "/map",
+    caption: "Карта",
+  },
+  profile: {
+    id: "profile",
+    href: "/profile",
+    caption: "Профиль",
+  },
+  exit: {
+    id: "exit",
+    href: "/exit",
+    caption: "Выйти",
+  },
+  login: {
+    id: "login",
+    href: "/login",
+    caption: "Логин",
+  },
+  register: {
+    id: "register",
+    href: "/register",
+    caption: "Регистрация",
+  },
+};
